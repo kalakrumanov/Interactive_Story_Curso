31b78071ee18adde3472b91903d5c25c7ec9702e
Kalak <kalak@Kalaks-BenQ.local>
2015-12-19 00:17:07 +0100

    Todo ok. Solo 2 acivities creadas, no se ve nada en la segunda. Es un commit de prueba, el primero desde Android Studio

---
+++ app/src/main/java/com/example/kalak/interactivestory/StoryActivity.java
@@ -0,0 +1,31 @@
+package com.example.kalak.interactivestory;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+public class StoryActivity extends AppCompatActivity {
+
+    public static final String TAG = StoryActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra(getString(R.string.key_name));
+
+        if (name == null) {
+            name = "nombre";
+        }
+        Log.d(TAG, name);
+
+    }
+
+}
