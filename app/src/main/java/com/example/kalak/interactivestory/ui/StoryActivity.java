bbb0f8ca56e778f134823930ad27b0a8ed7965cd
kalakrumanov <viktorbeat@yandex.com>
2015-12-19 20:13:45 +0100

    Ok. Se ve la segunda página estática.

--- app/src/main/java/com/example/kalak/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/com/example/kalak/interactivestory/ui/StoryActivity.java
@@ -1,15 +1,32 @@
 package com.example.kalak.interactivestory.ui;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.kalak.interactivestory.R;
+import com.example.kalak.interactivestory.model.Page;
+import com.example.kalak.interactivestory.model.Story;
 
 public class StoryActivity extends AppCompatActivity {
 
     public static final String TAG = StoryActivity.class.getSimpleName();
+
+    private Story mStory = new Story();
+    private ImageView mImageView;
+    private TextView mTextView;
+    private Button mChoice1;
+    private Button mChoice2;
+    private String mName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +34,42 @@ public class StoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_story);
 
         Intent intent = getIntent();
-        String name = intent.getStringExtra(getString(R.string.key_name));
+        mName = intent.getStringExtra(getString(R.string.key_name));
 
-        if (name == null) {
-            name = "nombre";
+        if (mName == null) {
+            mName = "nombre";
         }
 
-        //Log.d(TAG, name);
+        Log.d(TAG, mName);
+
+        mImageView = (ImageView) findViewById(R.id.storyImageView);
+        mTextView = (TextView) findViewById(R.id.storyTextView);
+        mChoice1 = (Button) findViewById(R.id.choiceButton1);
+        mChoice2 = (Button) findViewById(R.id.choiceButton2);
+
+        loadPage();
+    }
+
+
+    private void loadPage() {
+        Page page = mStory.getPage(1);
+
+
+        //Drawable drawable = ResourcesCompat.getDrawable(getResources(), page.getmImageId(), null);
+        //Drawable drawable = ContextCompat.getDrawable(this, page.getmImageId());
+        Drawable drawable = ContextCompat.getDrawable(this, R.drawable.page1);
+        mImageView.setImageDrawable(drawable);
+
+
+        String pageText = page.getmText();
+
+        // Añade si tenemos un placeholder "%1", y si no, nada
+        pageText = String.format(pageText,mName);
+
+        mTextView.setText(pageText);
+
+        mChoice1.setText(page.getmChoice1().getmText());
+        mChoice2.setText(page.getmChoice2().getmText());
 
     }
 
